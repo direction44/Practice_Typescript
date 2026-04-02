9a24181d1e39dc47cdc3461a9133b0610b3fd862
Disha Shivhare <dishashivhare44@gmail.com>
2026-04-02 17:36:47 +0530

    addinge exersice example2

---
+++ exercises/example1/dist/index2.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function swapPair(pairs) {
+    let [a, b] = pairs;
+    return [b, a];
+}
+console.log(swapPair([5, 6]));
+//# sourceMappingURL=index2.js.map
---
+++ exercises/example1/src/index2.ts
@@ -0,0 +1,6 @@
+function swapPair(pairs:[number,number])
+{
+    let [a,b]=pairs
+    return [b,a]
+}
+console.log(swapPair([5,6]))
